6d8f76ad9f3c9ea1b57b22ab76c560bad258ac9c
gustavorique <gustavorique@live.com>
2023-12-04 22:33:06 -0300

    feat: criação do perfil de dev

---
+++ src/main/java/com/book/store/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.book.store.config;
+
+import com.book.store.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public Boolean instanciaBaseDedados() {
+        if(strategy.equals("create")) {
+            dbService.instanciaBaseDeDados();
+        }
+        return false;
+    }
+
+}
